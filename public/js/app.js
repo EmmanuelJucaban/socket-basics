8fe8001dc00fae39e44c4cb572bedf88e077fc73
Emmanuel <emmanueljucaban@gmail.com>
2015-12-20 14:35:51 -0800

    Show timestamp on UI

--- public/js/app.js
+++ public/js/app.js
@@ -6,9 +6,10 @@ socket.on('connect', function() {
 
 
 socket.on('message', function(message) {
+	var momentTimestamp = moment.utc(message.timestamp);
     console.log('New message:');
     console.log(message.text);
-    $('.messages').append('<p>' + message.text + '</p>');
+    $('.messages').append('<p><strong>' + momentTimestamp.local().format('h:mm a') + '</strong>' + ' ' + message.text + '</p>');
 });
 
 
